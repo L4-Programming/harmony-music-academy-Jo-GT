5d05958beb0698ab9688ceae457153dc876bb0bb
outjinx3d <2145959@hc.ac.je>
2024-11-07 11:10:10 +0000

    Add comment plan

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-// javascript
-
-alert("Hello from index.js!");
+// Display a form to the user
+// Include input fields for the user's email address, level, and hours of study
+// Capture user's input on form submission
+  // Get the user's email address -userEmail (string)
+  // Get the user's level -userLevel (string)
+  // Get the user's hours of study -userHours (number)
+  // Validate the user's input
+  // Check if the user has selected a level  - check a level has been entered;check against the allowed levels; conditional statement
+  // Check if the user has provided an email address -@ symbol present, have they entered an email?;is it valid?
+  // Check if the user has specified at least one hour of study - cannot be negative
+  // Check if the number of hours requested is within the allowed range - have a 1hr minimum requirement; equal to or greater than 1;less than or equal to the maximum number for the level
+  // Calculate the total cost
+// Display the total cost to the user
