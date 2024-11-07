c88833e15119a1e3afcf1d3d217b8f2fb3c89b74
outjinx3d <2145959@hc.ac.je>
2024-11-07 12:17:04 +0000

    Added IDs to HTML and event listener to js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-// Display a form to the user
-// Include input fields for the user's email address, level, and hours of study
 // Capture user's input on form submission
-  // Get the user's email address -userEmail (string)
-  // Get the user's level -userLevel (string)
-  // Get the user's hours of study -userHours (number)
-  // Validate the user's input
-  // Check if the user has selected a level  - check a level has been entered;check against the allowed levels; conditional statement
-  // Check if the user has provided an email address -@ symbol present, have they entered an email?;is it valid?
-  // Check if the user has specified at least one hour of study - cannot be negative
-  // Check if the number of hours requested is within the allowed range - have a 1hr minimum requirement; equal to or greater than 1;less than or equal to the maximum number for the level
-  // Calculate the total cost
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  console.log("Form submitted");
+  let userEmail = document.querySelector("#email").value;
+  console.log({ userEmail });
+});
+// Store the user's email address as userEmail (string/text)
+// Store the user's level as userLevel (string/text)
+// Store the user's hours of study as userHours (number)
+// Validate the user's input
+// Check if the user has selected a level
+// Check if the user has provided an email address
+// Check if the user has specified at least one hour of study
+// Check if the number of hours requested is within the allowed range
+// Calculate the total cost
 // Display the total cost to the user
